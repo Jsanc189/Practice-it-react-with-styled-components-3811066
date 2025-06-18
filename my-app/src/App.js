a834989a191a8c4dc925f4e3198548c9d8eb7826
Jsanc189 <146498952+Jsanc189@users.noreply.github.com>
2025-06-18 23:23:33 +0000

    adds link page links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,38 @@ import logo from "./logo.svg";
 import "./App.css";
 import Button from "./components/examples/01_03"
 import DynamicUpdateComp from "./components/counter.js"
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact'
 
 const App = () => {
   return (
     <div>
-      <DynamicUpdateComp />
+      <h1> Welcome to My React App!</h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/" Home></Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">contact</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   )
 };
