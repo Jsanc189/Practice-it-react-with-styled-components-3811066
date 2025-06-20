c1f969e93b4dd0470cfc2806b9d01fe394c17032
Jsanc189 <146498952+Jsanc189@users.noreply.github.com>
2025-06-20 22:51:48 +0000

    styles books list with css styled components

--- my-app/src/components/BookDetails.js
+++ my-app/src/components/BookDetails.js
@@ -1,13 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
 
 const BookDetails = ({ book }) => {
-  return <div>
-    <h2>{book.title}</h2>
+  return <BookContainer>
+    <BookTitle>{book.title}</BookTitle>
     <p>Author: {book.author}</p>
     <p>Year: {book.year}</p>
-  </div>
+  </BookContainer>
 }
 
+const BookContainer = styled.div`
+  background-color: white;
+  width: 300px;
+  border: 2px solid black;
+  border-radius: 10px;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  justify-self: center;
+`
 
+const BookTitle = styled.h2`
+  text-align: center;
+`
 export default BookDetails;
--- my-app/src/components/BookList.js
+++ my-app/src/components/BookList.js
@@ -1,18 +1,40 @@
 import React from "react"
 import BookDetails from "./BookDetails";
 import books from "./challanges/bookData"
+import styled from "styled-components";
+
 const BookList = () => {
   return (
-    <div>
-      <h1>My Reading List</h1>
-      {books.map(book => (
-        <BookDetails key={book.id} book={book} />
-      ))}
-    </div>
+    <Wrapper>
+      <Title>My Reading List</Title>
+      <Grid>
+        {books.map(book => (
+          <BookDetails key={book.id} book={book} />
+        ))}
+      </Grid>
+    </Wrapper>
   );
 };
 
+const Wrapper = styled.div`
+  height: 100vh;
+  overflow: scroll;
+  background-color: lightgray;
+`
 
+const Title = styled.h1`
+  margin: 0;
+  color: darkgreen;
+  text-align: center;
+`
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: auto;
+  align-items: center;
+  justify-content:start;
+  gap: 5%;  
+`
 
 export default BookList;
